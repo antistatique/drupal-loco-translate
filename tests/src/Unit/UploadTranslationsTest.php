6195c5c4bc549aa1e68625df5a0ba3a25ab5b8ea
Gilles Doge <gilles@antistatique.net>
2019-06-07 19:01:37 +0200

    UploadTranslation services

---
+++ tests/src/Unit/UploadTranslationsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\loco_translate\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+use Drupal\loco_translate\UploadTranslations;
+use Loco\Http\ApiClient;
+
+/**
+ * @coversDefaultClass \Drupal\loco_translate\UploadTranslations
+ *
+ * @group loco_translate
+ * @group loco_translate_unit
+ * @group loco_translate_unit_upload
+ */
+class UploadTranslationsTest extends UnitTestCase
+{
+    /**
+     * @var \Drupal\loco_translate\UploadTranslations
+     */
+    private $uploader;
+    private $apiClient;
+
+
+    public function setUp() {
+        $this->apiClient = $this->prophetize(ApiClient::class);
+
+        $this->uploader = new UploadTranslations($this->apiClient->reveal());
+    }
+
+    public function testUploadFile() {
+
+        $file = __DIR__ . '/../../modules/loco_translate_test/assets/fr.po';
+
+        $this->apiClient->expect()->with('import', [
+            'data' => 'LOT OF RAW DATA. TODO',
+            'locale' => 'fr',
+            'ext' => 'po',
+            // ...
+        ]);
+
+        $result = $this->uploader->uploadFile($file, 'fr');
+    }
+
+}
