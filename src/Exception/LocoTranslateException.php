caedbc6d0131880bf31c3577a92ba806c79a5982
Kevin Wenger <wenger.kev@gmail.com>
2019-06-11 10:37:01 +0200

    add new exception for read, write, access file/path

--- src/Exception/LocoTranslateException.php
+++ src/Exception/LocoTranslateException.php
@@ -14,10 +14,49 @@ class LocoTranslateException extends \Exception {
    *   The ISO langcode which is invalid.
    *
    * @return LocoTranslateException
-   *   The invalid langcode exception.
+   *   Exception used when the langcode is not enabled/available on Drupal.
    */
   public static function invalidLangcode($langcode) {
     return new static('The langcode ' . $langcode . ' is not defined. Please create & enabled it before trying to use it.');
+  }
+
+  /**
+   * The path or file does not exist.
+   *
+   * @param string $path
+   *   The file or path.
+   *
+   * @return LocoTranslateException
+   *   Exception used when the given path or file does not exist.
+   */
+  public static function notFound($path) {
+    return new static('No such file or directory ' . $path);
+  }
+
+  /**
+   * The file is not readalbe.
+   *
+   * @param string $file
+   *   The file to read.
+   *
+   * @return LocoTranslateException
+   *   Exception used when the file is not readable.
+   */
+  public static function isNotReadable($file) {
+    return new static('The file ' . $file . ' is not readable.');
+  }
+
+  /**
+   * The path is not writable.
+   *
+   * @param string $path
+   *   The path.
+   *
+   * @return LocoTranslateException
+   *   Exception used when the path is not writable.
+   */
+  public static function isNotWritable($path) {
+    return new static('The path ' . $path . ' is not writable.');
   }
 
 }
