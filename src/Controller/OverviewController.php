5c87ae67759b88408f325fd492b38a4d4c7801c3
Kevin Wenger <wenger.kev@gmail.com>
2019-06-11 16:47:55 +0200

    add empty loco translations dashboard

---
+++ src/Controller/OverviewController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\loco_translate\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Loco dashboard overview.
+ */
+class OverviewController extends ControllerBase {
+
+  /**
+   * Shows the dashboard screen.
+   *
+   * @return array
+   *   The render array for the dashboard screen.
+   */
+  public function dashboard() {
+    return [];
+  }
+
+}
