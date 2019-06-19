dc1d991661e9e50b333cb7d6260a4f945e05d857
Kevin Wenger <wenger.kev@gmail.com>
2019-06-19 14:27:28 +0200

    add utility methodes to store last push & push

--- src/Utility.php
+++ src/Utility.php
@@ -3,6 +3,7 @@
 namespace Drupal\loco_translate;
 
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\State\StateInterface;
 
 /**
  * Contains utility methods for the Loco Translate module.
@@ -16,13 +17,23 @@ class Utility {
   protected $languageManager;
 
   /**
+   * The state service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
    * Class constructor.
    *
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service.
    */
-  public function __construct(LanguageManagerInterface $language_manager) {
+  public function __construct(LanguageManagerInterface $language_manager, StateInterface $state) {
     $this->languageManager = $language_manager;
+    $this->state = $state;
   }
 
   /**
@@ -51,6 +62,34 @@ class Utility {
   public function isLangcodeEnabled($langcode) {
     $languages = $this->languageManager->getLanguages();
     return isset($languages[$langcode]);
+  }
+
+  /**
+   * Set the last pull time by given langcode.
+   *
+   * @param string $langcode
+   *   The locale to use.
+   * @param string $time
+   *   The time to save.
+   */
+  public function setLastPull($langcode, $time) {
+    $pull_last = (array) $this->state->get('loco_translate.api.pull_last');
+    $pull_last[$langcode] = $time;
+    $this->state->set('loco_translate.api.pull_last', $pull_last);
+  }
+
+  /**
+   * Set the last push time by given langcode.
+   *
+   * @param string $langcode
+   *   The locale to use.
+   * @param string $time
+   *   The time to save.
+   */
+  public function setLastPush($langcode, $time) {
+    $push_last = (array) $this->state->get('loco_translate.api.push_last');
+    $push_last[$langcode] = $time;
+    $this->state->set('loco_translate.api.push_last', $push_last);
   }
 
 }
--- tests/src/Unit/UtilityTest.php
+++ tests/src/Unit/UtilityTest.php
@@ -37,4 +37,22 @@ class UtilityTest extends UtilityTestBase {
     ];
   }
 
+  /**
+   * @covers \Drupal\loco_translate\Utility::setLastPull
+   */
+  public function testLastPush() {
+    $this->markTestIncomplete(
+      'This test has not been implemented yet.'
+    );
+  }
+
+  /**
+   * @covers \Drupal\loco_translate\Utility::setLastPull
+   */
+  public function testLastPull() {
+    $this->markTestIncomplete(
+      'This test has not been implemented yet.'
+    );
+  }
+
 }
